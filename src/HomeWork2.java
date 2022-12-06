2f1a1d49f9078d04c685edf5903907d8038b4d02
Vovka <lumen_08>
2022-12-06 20:51:41 +0300

    HomeWork2_complite

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -1,4 +1,6 @@
+import java.io.IOException;
 import java.util.Objects;
+import java.util.logging.*;
 
 public class HomeWork2 {
     public static StringBuilder task1(String text) {
@@ -30,5 +32,34 @@ public class HomeWork2 {
                             .append(arr[i + 2] + " ");
         }
         return new StringBuilder(sb.toString().trim());
+    }
+
+    public static void task2 () throws IOException {
+        Logger log = Logger.getLogger(HomeWork2.class.getName());
+        FileHandler fh = new FileHandler("log.txt");
+        log.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+
+        int temp;
+        boolean isSorted = false;
+
+        int[] num = {1, 78, 48, 5, 22, 10, 3};
+        while (!isSorted) {
+            log.log(Level.INFO,"Вход в цикл While");
+            isSorted = true;
+            for (int i = 0; i < num.length - 1; i++) {
+                if (num[i] > num[i + 1]) {
+                    log.log(Level.WARNING,"Пемещение индексов массива");
+                    isSorted = false;
+                    temp = num[i];
+                    num[i] = num[i + 1];
+                    num[i + 1] = temp;
+                }
+            }
+        }
+        for (int res: num) {
+            System.out.printf("%d ",res);
+            }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
     
-    public static void main(String[] args) {
-        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
-        System.out.print(HomeWork2.task1(json));
+    public static void main(String[] args) throws IOException {
+        // Запуск первой задачи.
+//        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+//        System.out.print(HomeWork2.task1(json));
+        // Запуск второй задачи.
+//        HomeWork2.task2();
     }
 }
 
