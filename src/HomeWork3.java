3cbd453e200c64f360ac83f3453f5e4ba9d3ae5e
Vovka <lumen_08>
2022-12-08 20:55:25 +0300

    HM3

---
+++ src/HomeWork3.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HomeWork3{
+    public static void task1(){
+        List<Integer> arrayNum = new ArrayList<>();
+        List<Integer> arrayNum2 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayNum.add((int) (Math.random()*100));
+            arrayNum2.add((int) (Math.random()*100));
+        }
+
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,7 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) throws IOException {
-        // Запуск первой задачи.
-//        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
-//        System.out.print(HomeWork2.task1(json));
-        // Запуск второй задачи.
-//        HomeWork2.task2();
+        HomeWork3.task1();
     }
 }
 
